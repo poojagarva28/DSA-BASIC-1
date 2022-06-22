a4526f00a5b13b41cb5c6e2203d7efe23d56d258
Pooja Garva <42610074+poojagarva28@users.noreply.github.com>
2022-06-22 13:17:24 +0530

    Add files via upload

--- 9.js
+++ 9.js
@@ -1,41 +1,44 @@
-console.clear();
-
 class Stack {
   constructor() {
     this.items = [];
+    this.reverse = [];
     this.length = 0;
-    this.push = (element) => {
-      this.items.push(element);
+    this.push = (ele) => {
+      this.items.push(ele);
       this.length += 1;
     };
     this.pop = () => {
-      if (this.length == 0) {
+      if (this.length === 0) {
         return -1;
       }
       this.length -= 1;
       return this.items.pop();
     };
-    this.peek = () => {
-      if (this.length == 0) {
+    this.peak = () => {
+      if (this.length === 0) {
         return -1;
       }
       return this.items[this.length - 1];
     };
+    this.addtoReverse = (ele) => {
+      let n = ele.length;
+      for (let i = 0; i < n; i++) {
+        this.reverse.push(ele.pop());
+      }
+      return this.reverse;
+    };
   }
 }
 
-let stack = new Stack();
-
-let arr = [13, 11, 8, 4, 19, 22];
-let rev = [];
-
-for (let i = 0; i < arr.length; i++) {
-  stack.push(arr[i]);
-}
-console.log("input stack:", stack.items);
-for (let i = 0; i < arr.length; i++) {
-  rev.push(stack.pop());
-  //   arr[i] = stack.pop();
-}
-
-console.log("reversed stack:", rev);
+let s = new Stack();
+s.push(10);
+s.push(20);
+s.push(30);
+s.push(40);
+console.log("stack items:", s.items);
+console.log("stack length:", s.length);
+console.log("popped element:", s.pop());
+console.log("stack items after pop:", s.items);
+s.push(50);
+console.log("stack items after push:", s.items);
+console.log("reverse stack:", s.addtoReverse(s.items));
