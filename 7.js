0444ad242bcceef73dd3f9ff8dfc8780fabb874b
Pooja Garva <42610074+poojagarva28@users.noreply.github.com>
2022-06-22 12:42:13 +0530

    Add files via upload

--- 7.js
+++ 7.js
@@ -1,40 +1,75 @@
-function isOperator(x) {
-  switch (x) {
+console.clear();
+
+class Stack {
+  constructor() {
+    this.items = [];
+    this.size = 0;
+    this.push = (ele) => {
+      this.items.push(ele);
+      this.size++;
+    };
+    this.pop = () => {
+      if (this.size === 0) {
+        return -1;
+      } else {
+        this.size--;
+        return this.items.pop();
+      }
+    };
+    this.peak = () => {
+      if (this.size === 0) {
+        return -1;
+      } else {
+        return this.items[this.size - 1];
+      }
+    };
+  }
+}
+
+const isOperator = (ele) => {
+  switch (ele) {
     case "+":
     case "-":
     case "*":
     case "/":
-    case "^":
-    case "%":
       return true;
   }
   return false;
-}
+};
 
-function prefix_to_infix(str) {
-  let stack = [];
-
-  let l = str.length;
-
-  for (let i = l - 1; i >= 0; i--) {
-    let c = str[i];
-
-    if (isOperator(c)) {
-      let op1 = stack[stack.length - 1];
-      stack.pop();
-      let op2 = stack[stack.length - 1];
-      stack.pop();
-
-      let temp = "(" + op1 + c + op2 + ")";
-      stack.push(temp);
+const pre_to_infix = (pre_fix) => {
+  let s = new Stack();
+  let n = pre_fix.length;
+  for (let i = n - 1; i >= 0; i--) {
+    if (isOperator(pre_fix[i])) {
+      let op1 = s.items[s.size - 1];
+      s.pop();
+      let op2 = s.items[s.size - 1];
+      s.pop();
+      let temp = "(" + op1 + pre_fix[i] + op2 + ")";
+      s.push(temp);
     } else {
-      stack.push(c + "");
+      s.push(pre_fix[i] + "");
     }
   }
-  return stack[stack.length - 1];
-}
+  let ans = "";
+  while (s.size > 0) {
+    ans += s.pop();
+  }
+  return ans;
+};
 
-let exp = "*-AB/CD-/E";
+let pre_fix = "*-A/BC-/AKL";
+console.log(`given prefix expression : ${pre_fix}`);
+console.log(`infix expression of it : ${pre_to_infix(pre_fix)}`);
 
-console.log(`given prefix expression : ${exp}`);
-console.log(`infix expression of it : ${prefix_to_infix(exp)}`);
+/* STEPS 
+Read the Prefix expression in reverse order (from right to left)
+If the symbol is an operand, then push it onto the Stack
+If the symbol is an operator, then pop two operands from the Stack 
+Create a string by concatenating the two operands and the operator between them. 
+string = (operand1 + operator + operand2) 
+And push the resultant string back to Stack
+Repeat the above steps until the end of Prefix expression.
+At the end stack will have only 1 string i.e resultant string
+*/
